19eaf4b3ebbabc8aa22c697ca21b675a2f927c66
uniyalricha-dev <142781389+uniyalricha-dev@users.noreply.github.com>
2023-09-08 15:32:34 +0530

    Update variables.js
    table

--- variables.js
+++ variables.js
@@ -3,6 +3,11 @@ let accountEmail= "richa@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 
-// accountId = 2
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121"
+accountCity = "Bengaluru"
 
 console.log(accountId);
+console. table([accountId, accountEmail,  accountPassword, accountCity]);
