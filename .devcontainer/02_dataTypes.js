2d387a0a4e76a012db53fff75f83ad9fdb28f3ee
uniyalricha-dev <142781389+uniyalricha-dev@users.noreply.github.com>
2023-09-12 14:02:57 +0530

    datatypes

--- .devcontainer/02_dataTypes.js
+++
@@ -1,8 +0,0 @@
-"use strict"; // treat all JS code as newer version
-
-//alert(3+2)// we are using nodejs, not browser
-
-console.log( 3
-    +2); 
-
-console.log("Richa");
---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as never version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    2) // code readability should be high
+
+console.log("Richa")
+
+let name="Richa"
+let age=19
+let isloggedIn= false
+let state;
+
+// number => 2 to power 53
+// bigint( used for large number)
+// string =>""
+// boolean => true/false
+//null =>standalone value
+// undefined =>
+// symbol =>unique
+
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
