2dfdc59a2b6d255941ec751835bef7756c075453
uniyalricha-dev <142781389+uniyalricha-dev@users.noreply.github.com>
2023-09-13 14:00:54 +0530

    conversion

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -6,3 +6,12 @@ console.log(typeof(score));
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+//"45" => 45
+// "45abc" => NaN
+// true =>1; false => 0
+
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(IsloggedIn)
+console.log(booleanIsloggedIn);
